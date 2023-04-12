9a0c1ef1fb5a9cedebc04bae3f1155649639b5ee
Mykola Kholtobin <nik.kholtobin@gmail.com>
2023-04-12 18:51:55 +0300

    Optimize code.

---
+++ image_processor/extract_text_service.rb
@@ -0,0 +1,29 @@
+class ExtractTextService
+  def call(url)
+    # Download the image
+    image_tempfile = Tempfile.new(['image', File.extname(url)])
+    File.open(image_tempfile.path, 'wb') do |file|
+      file.write(HTTP.get(url).to_s)
+    end
+
+    # Perform OCR processing
+    result = perform_ocr(image_tempfile.path)
+
+    # Log the result or an error
+    if result
+      [:ok, "Text found: #{result}"]
+    else
+      [:error]
+    end
+  ensure
+    # Clean up
+    image_tempfile.close
+    image_tempfile.unlink
+  end
+
+  private
+
+  def perform_ocr(image_path)
+    RTesseract.new(image_path).to_s
+  end
+end
--- image_processor/start.rb
+++ image_processor/start.rb
@@ -1,6 +1,8 @@
 require 'http'
 require 'tempfile'
 require 'bunny'
+require 'rtesseract'
+require './extract_text_service'
 
 class OcrWorker
   def start
@@ -12,59 +14,18 @@ class OcrWorker
   private
 
   def process_image(file_url)
-    # Download the image
-    image_tempfile = Tempfile.new(['image', File.extname(file_url)])
-    File.open(image_tempfile.path, 'wb') do |file|
-      file.write(HTTP.get(file_url).to_s)
+    case ExtractTextService.new.call(file_url)
+    in [:ok, text] then puts  "Text found: #{text}"
+    in [:error, _] then puts "Error: OCR processing failed."
     end
-
-    # Perform OCR processing
-    result = perform_ocr(image_tempfile.path)
-
-    # Log the result or an error
-    if result
-      puts "Text found: #{result}"
-    else
-      puts "Error: OCR processing failed."
-    end
-
-    # Clean up
-    image_tempfile.close
-    image_tempfile.unlink
-  end
-
-  def rabbitmq_connection
-    @rabbitmq_connection ||= begin
-      connection = Bunny.new(host: 'rabbitmq', user: 'guest', pass: 'guest')
-      connection.start
-      connection
-    end
-  end
-
-  def rabbitmq_channel
-    @rabbitmq_channel ||= rabbitmq_connection.create_channel
   end
 
   def ocr_queue
-    @ocr_queue ||= rabbitmq_channel.queue('ocr')
+    rabbitmq_connection = Bunny.new(host: 'rabbitmq', user: 'guest', pass: 'guest').tap(&:start)
+    rabbitmq_channel = rabbitmq_connection.create_channel
+
+    rabbitmq_channel.queue('ocr')
   end
-
-
-
-  def perform_ocr(image_path)
-    # Use an OCR library to recognize text from the image.
-    # You can use the Tesseract OCR library with the "tesseract" gem or another OCR library.
-    # For this example, we will return dummy text as the recognized text.
-    puts "Sample text from image "
-  end
-
-=begin
-  def perform_ocr(image_path)
-    # Use the Tesseract OCR library to recognize text from the image.
-    ocr = RTesseract.new(image_path, lang: 'eng')
-    ocr.to_s # returns the recognized text
-  end
-=end
 end
 
 OcrWorker.new.start
